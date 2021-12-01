5b8af173545e532c1fae320c1e9af06252021188
DAI KOI YIM 2001062E <2001062E@student.tp.edu.sg>
2021-12-01 12:30:13 +0800

    Add project files.

---
+++ CarRentalManagement/Server/Data/Migrations/20211124023002_AddedDefaultDataAndUser.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CarRentalManagement.Server.Data.Migrations
+{
+    public partial class AddedDefaultDataAndUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Vehicles_Makes_MakeId",
+                table: "Vehicles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Vehicles_Models_ModelId",
+                table: "Vehicles");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Models",
+                table: "Models");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Makes",
+                table: "Makes");
+
+            migrationBuilder.DropColumn(
+                name: "CustomerID",
+                table: "Customers");
+
+            migrationBuilder.RenameTable(
+                name: "Models",
+                newName: "Model");
+
+            migrationBuilder.RenameTable(
+                name: "Makes",
+                newName: "Make");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Model",
+                table: "Model",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Make",
+                table: "Make",
+                column: "Id");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "ad2bcf0c-20db-474f-8407-5a6b159518ba", "2d9bbf70-cbed-41e7-8def-911480dfbc8e", "Administrator", "ADMINISTRATOR" },
+                    { "bd2bcf0c-20db-474f-8407-5a6b159518bb", "f6cae57b-0b14-4314-9763-948f782ea9b1", "User", "USER" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "FirstName", "LastName", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "3781efa7-66dc-47f0-860f-e506d04102e4", 0, "13b75018-4df1-4109-8af6-717b8067bb97", "admin@localhost.com", false, "Admin", "User", false, null, "ADMIN@LOCALHOST.COM", "ADMIN", "AQAAAAEAACcQAAAAEPfy0VsMZH4IufpJD1vO9lBZs+P3vXsqpiajDmY6YoDxgyQ3eXolAnREsp1WLliEPw==", null, false, "aca5c14a-76e8-4fa1-bf29-088e1c44b1ed", false, "Admin" });
+
+            migrationBuilder.InsertData(
+                table: "Colours",
+                columns: new[] { "Id", "CreatedBy", "DateCreated", "DateUpdated", "Name", "UpdatedBy" },
+                values: new object[,]
+                {
+                    { 1, "System", new DateTime(2021, 11, 24, 10, 30, 1, 991, DateTimeKind.Local).AddTicks(8709), new DateTime(2021, 11, 24, 10, 30, 1, 993, DateTimeKind.Local).AddTicks(1728), "Black", "System" },
+                    { 2, "System", new DateTime(2021, 11, 24, 10, 30, 1, 993, DateTimeKind.Local).AddTicks(2646), new DateTime(2021, 11, 24, 10, 30, 1, 993, DateTimeKind.Local).AddTicks(2651), "Blue", "System" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Make",
+                columns: new[] { "Id", "CreatedBy", "DateCreated", "DateUpdated", "Name", "UpdatedBy" },
+                values: new object[,]
+                {
+                    { 1, "System", new DateTime(2021, 11, 24, 10, 30, 1, 994, DateTimeKind.Local).AddTicks(6607), new DateTime(2021, 11, 24, 10, 30, 1, 994, DateTimeKind.Local).AddTicks(6621), "BMW", "System" },
+                    { 2, "System", new DateTime(2021, 11, 24, 10, 30, 1, 994, DateTimeKind.Local).AddTicks(6626), new DateTime(2021, 11, 24, 10, 30, 1, 994, DateTimeKind.Local).AddTicks(6628), "Toyota", "System" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Model",
+                columns: new[] { "Id", "CreatedBy", "DateCreated", "DateUpdated", "Name", "UpdatedBy" },
+                values: new object[,]
+                {
+                    { 1, "System", new DateTime(2021, 11, 24, 10, 30, 1, 995, DateTimeKind.Local).AddTicks(2004), new DateTime(2021, 11, 24, 10, 30, 1, 995, DateTimeKind.Local).AddTicks(2023), "3 Series", "System" },
+                    { 2, "System", new DateTime(2021, 11, 24, 10, 30, 1, 995, DateTimeKind.Local).AddTicks(2029), new DateTime(2021, 11, 24, 10, 30, 1, 995, DateTimeKind.Local).AddTicks(2032), "X5", "System" },
+                    { 3, "System", new DateTime(2021, 11, 24, 10, 30, 1, 995, DateTimeKind.Local).AddTicks(2037), new DateTime(2021, 11, 24, 10, 30, 1, 995, DateTimeKind.Local).AddTicks(2039), "Prius", "System" },
+                    { 4, "System", new DateTime(2021, 11, 24, 10, 30, 1, 995, DateTimeKind.Local).AddTicks(2044), new DateTime(2021, 11, 24, 10, 30, 1, 995, DateTimeKind.Local).AddTicks(2045), "Rav4", "System" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "ad2bcf0c-20db-474f-8407-5a6b159518ba", "3781efa7-66dc-47f0-860f-e506d04102e4" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Vehicles_Make_MakeId",
+                table: "Vehicles",
+                column: "MakeId",
+                principalTable: "Make",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Vehicles_Model_ModelId",
+                table: "Vehicles",
+                column: "ModelId",
+                principalTable: "Model",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Vehicles_Make_MakeId",
+                table: "Vehicles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Vehicles_Model_ModelId",
+                table: "Vehicles");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Model",
+                table: "Model");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Make",
+                table: "Make");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "bd2bcf0c-20db-474f-8407-5a6b159518bb");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "ad2bcf0c-20db-474f-8407-5a6b159518ba", "3781efa7-66dc-47f0-860f-e506d04102e4" });
+
+            migrationBuilder.DeleteData(
+                table: "Colours",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Colours",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Make",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Make",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Model",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Model",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Model",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Model",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "ad2bcf0c-20db-474f-8407-5a6b159518ba");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "3781efa7-66dc-47f0-860f-e506d04102e4");
+
+            migrationBuilder.RenameTable(
+                name: "Model",
+                newName: "Models");
+
+            migrationBuilder.RenameTable(
+                name: "Make",
+                newName: "Makes");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CustomerID",
+                table: "Customers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Models",
+                table: "Models",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Makes",
+                table: "Makes",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Vehicles_Makes_MakeId",
+                table: "Vehicles",
+                column: "MakeId",
+                principalTable: "Makes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Vehicles_Models_ModelId",
+                table: "Vehicles",
+                column: "ModelId",
+                principalTable: "Models",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
